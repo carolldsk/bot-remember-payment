e385097b8788ccfc0346a231fe2f2e0b6a4fc9d0
Ana Carolina Vieira Batista <ana.batista@estadao.com>
2022-01-31 21:05:28 -0300

    improving the code

--- main.js
+++ main.js
@@ -1,58 +1,59 @@
 const { app, BrowserWindow, ipcMain, remote} = require('electron')
 const express = require("express");
-const path = require('path');
 
 app.whenReady().then(() => {
 
-  var ex = express();
+    var ex = express();
 
-  var mainWindow = new BrowserWindow(
-
-    {width: 1020, height: 600,webPreferences:{
-
-      nodeIntegration: true      },
-
-  });
-
-  mainWindow.loadFile('index.html');
-
-
-  ex.get("/whats/:num/:msg", async (req,res) => {
-    var phone   = req.params.num;
-    var message = req.params.msg;
-
-    enviar(phone,message);
-    res.send("Sending a message by Whatsapp..")
-  });
-
-  async function enviar(phone,message){
-  
-    mainWindow.loadURL("https://web.whatsapp.com/send?phone="+phone+"&text="+message, 
-    { userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36' });
-
-    mainWindow.webContents.once('dom-ready', () => {
-
-        mainWindow.webContents.executeJavaScript(`
-                console.log("This loads no problem!");
-                var enviado = false;
-                
-                function tempo(){
-                
-                    var btsend = document.getElementsByClassName("_4sWnG")[0];
-                    var inputSend = document.getElementsByClassName("_13NKt")[1];
-                    
-                    if(typeof inputSend !== "undefired" && inputSend.textContent && !enviado){
-                        btsend.click();
-                        enviado = true;
-                      
-                    }else if(enviado) {
-                    ${mainWindow.show()}
-                        enviado = false;
-                    }
-                }
-                setInterval(tempo, 5000);
-        `);
+    var mainWindow = new BrowserWindow({
+        width: 1020, 
+        height: 600,
+        webPreferences:{nodeIntegration: true},
     });
-  }
-  ex.listen(3400);
+
+    mainWindow.loadFile('index.html');
+
+    // get url 
+    ex.get("/whats/:num/:msg", async (req,res) => {
+        var phone = req.params.num;
+        var name  = 'Ana';
+
+        let defaultMassage = `Oie, ${name}%0aEu sou seu novo Bot de alerta de pagamento!🤖%0a%0aTô passando para te lembrar que você precisa realizar o pagamento de *R$ 55,90* da sua conta da Netflix.%0a%0aNão esquece de enviar o comprovante de pagamento no grupo 😉%0aAté a próxima!`;
+
+        sendMessage(phone,defaultMassage);
+        res.send("Sending a message by Whatsapp..")
+    });
+
+    async function sendMessage(phone,defaultMassage){
+        mainWindow.loadURL("https://web.whatsapp.com/send?phone="+phone+"&text="+defaultMassage, 
+        { userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36' });
+
+        mainWindow.webContents.once('dom-ready', () => {
+
+            mainWindow.webContents.executeJavaScript(`
+                    console.log("This loads no problem!");
+                    var sent = false;
+                    
+                    function interval(){
+                    
+                        var btsend = document.getElementsByClassName("_4sWnG")[0];
+                        var inputSend = document.getElementsByClassName("_13NKt")[1];
+                        
+                        if(typeof inputSend !== "undefired" && inputSend.textContent && !sent){
+                            btsend.click();
+                            sent = true;
+                        
+                        }else if(sent) {
+                            ${mainWindow.show()}
+                            enviado = false;
+                        }
+                    }
+                    setInterval(interval, 5000);
+            `);
+        });
+    }
+
+    //listem port
+    ex.listen(3400);
+
 });
