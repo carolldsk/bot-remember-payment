311bc11085ca71d707dbd3a9f4565ea130cf40b8
Ana Carolina Vieira Batista <ana.batista@estadao.com>
2022-02-01 00:35:07 -0300

    separating responsibilities

--- main.js
+++ main.js
@@ -1,35 +1,46 @@
 const { app, BrowserWindow, ipcMain, remote} = require('electron')
 const express = require("express");
+const { getPayerName, getCurrentMonth, getCurrentDay  } = require('./resources/js/helper');
+const { dateRemember } = require('./resources/js/enum');
+const ex = express();
 
 app.whenReady().then(() => {
 
-    var ex = express();
-
-    var mainWindow = new BrowserWindow({
+    // build window
+    const mainWindow = new BrowserWindow({
         width: 1020, 
         height: 600,
         webPreferences:{nodeIntegration: true},
     });
 
+    // loading landing page
     mainWindow.loadFile('index.html');
 
-    // get url 
-    ex.get("/whats/:num/:msg", async (req,res) => {
-        var phone = req.params.num;
-        var name  = 'Ana';
+    (() => {
+        let date = getCurrentDay();
 
-        let defaultMassage = `Oie, ${name}%0aEu sou seu novo Bot de alerta de pagamento!🤖%0a%0aTô passando para te lembrar que você precisa realizar o pagamento de *R$ 55,90* da sua conta da Netflix.%0a%0aNão esquece de enviar o comprovante de pagamento no grupo 😉%0aAté a próxima!`;
+        if(dateRemember.includes(date)){
+            let month = getCurrentMonth();
+            let name = getPayerName(month);
 
-        sendMessage(phone,defaultMassage);
-        res.send("Sending a message by Whatsapp..")
-    });
+            //Get Phone Number
+            ex.get("/whats/:num", async (req,res) => {
+                var phone = req.params.num;
+                let defaultMassage = `Oie, ${name}%0aEu sou seu novo Bot de alerta de pagamento!🤖%0a%0aTô passando para te lembrar que você precisa realizar o pagamento de *R$ 55,90* da sua conta da Netflix.%0a%0aNão esquece de enviar o comprovante de pagamento no grupo 😉%0aAté a próxima!`;
+
+                sendMessage(phone,defaultMassage);
+                res.send("Sending a message by Whatsapp..")
+            });
+        }
+    })()
 
     async function sendMessage(phone,defaultMassage){
+    
         mainWindow.loadURL("https://web.whatsapp.com/send?phone="+phone+"&text="+defaultMassage, 
         { userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36' });
-
+    
         mainWindow.webContents.once('dom-ready', () => {
-
+    
             mainWindow.webContents.executeJavaScript(`
                     console.log("This loads no problem!");
                     var sent = false;
@@ -53,7 +64,9 @@ app.whenReady().then(() => {
         });
     }
 
+
     //listem port
     ex.listen(3400);
+
 
 });
